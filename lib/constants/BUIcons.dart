9c6d2ecf950923718b59740e1fa1bc50415df62c
Mahesh <maheshphuyal62@gmail.com>
2024-05-18 10:06:39 +0545

    change icon names and icon class

---
+++ lib/constants/BUIcons.dart
@@ -0,0 +1,39 @@
+class BUIcons {
+  // activity.svg
+  // arrow_left.svg
+  // camera.svg
+  // chat.svg
+  // delete.svg
+  // delete_1.svg
+  // filter_2.svg
+  // logout.svg
+  // message.svg
+  // more_circle.svg
+  // notification.svg
+  // paper.svg
+  // plus.svg
+  // search.svg
+  // send.svg
+  // voice_1.svg
+  // voice_2.svg
+
+  static String ACTIVITY = iconRoot + "activity.svg";
+  static String ARROW_LEFT = iconRoot + "arrow_left.svg";
+  static String CAMERA = iconRoot + "camera.svg";
+  static String CHAT = iconRoot + "chat.svg";
+  static String DELETE = iconRoot + "delete.svg";
+  static String DELETE_1 = iconRoot + "delete_1.svg";
+  static String FILTER_2 = iconRoot + "filter_2.svg";
+  static String LOGOUT = iconRoot + "logout.svg";
+  static String MESSAGE = iconRoot + "message.svg";
+  static String MORE_CIRCLE = iconRoot + "more_circle.svg";
+  static String NOTIFICATION = iconRoot + "notification.svg";
+  static String PAPER = iconRoot + "paper.svg";
+  static String PLUS = iconRoot + "plus.svg";
+  static String SEARCH = iconRoot + "search.svg";
+  static String SEND = iconRoot + "send.svg";
+  static String VOICE_1 = iconRoot + "voice_1.svg";
+  static String VOICE_2 = iconRoot + "voice_2.svg";
+
+  static String iconRoot = "assets/icons/";
+}
